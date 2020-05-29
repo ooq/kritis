8958de07b353fb33ce86915c925a02b69dd671a7
Qifan Pu <qifan.pu@gmail.com>
2020-05-29 18:16:01 -0400

    test.

--- integration/signer/signer_int.sh
+++ integration/signer/signer_int.sh
@@ -1,8 +1,24 @@
 # Signer integration testing script
 set -ex
 
-# helpers
-ls -al
+# build a "good" example image
+GOOD_IMAGE_URL=gcr.io/$PROJECT_ID/signer-int-good-image:$BUILD_ID
+
+docker build -t $GOOD_IMAGE_URL -f ./Dockerfile.good .
+delete_good_image () {
+    ARG=$?
+    echo "Delete good image."
+    gcloud container images delete $GOOD_IMAGE_URL --force-delete-tags \
+      --quiet
+    exit $ARG
+}
+trap delete_good_image EXIT
+
+docker push $GOOD_IMAGE_URL
+# get image url with digest format
+GOOD_IMG_DIGEST_URL=$(docker image inspect $GOOD_IMAGE_URL --format '{{index .RepoDigests 0}}')
+
+# build a "bad" example image
 
 
 # check project id
